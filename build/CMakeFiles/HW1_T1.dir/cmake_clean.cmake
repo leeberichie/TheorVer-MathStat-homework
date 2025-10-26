6649b6341d46f9995e2acbca89c8a91358419235
leeberichie <leeberichie@gmail.com>
2025-10-26 15:35:37 +0300

    Initial Commit

---
+++ build/CMakeFiles/HW1_T1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW1_T1.dir/main.cpp.o"
+  "CMakeFiles/HW1_T1.dir/main.cpp.o.d"
+  "HW1_T1"
+  "HW1_T1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW1_T1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
